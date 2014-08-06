c8fe9aa7daff8fea62c147b2648933fd2d3551b2
Wojciech Ciołko <w.ciolko@gmail.com>
2014-08-06 17:10:58 +0200

    AM-198 js refactoring

--- Resources/public/js/workflow-show.js
+++ Resources/public/js/workflow-show.js
@@ -1,16 +1,18 @@
-jQuery('#workflowTabs a').click(function (e) {
-    e.preventDefault();
-    var url = jQuery(this).attr("data-url");
-    var href = this.hash;
-    var pane = jQuery(this);
+jQuery(function () {
+    jQuery('#workflowTabs a').click(function (e) {
+        e.preventDefault();
+        var url = jQuery(this).attr("data-url");
+        var href = this.hash;
+        var pane = jQuery(this);
 
-    if (url) {
-        var l = Ladda.create(this);
-        l.start();
-    }
-    // ajax load from data-url
-    jQuery(href).load(url, function (result) {
-        pane.tab('show');
-        l.stop();
+        if (url) {
+            var l = Ladda.create(this);
+            l.start();
+        }
+        // ajax load from data-url
+        jQuery(href).load(url, function (result) {
+            pane.tab('show');
+            l.stop();
+        });
     });
 });
